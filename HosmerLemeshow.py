60940d77736216590ae6675d50117d5c25769bf6
TristanFauvel <tristan.fauvel@hotmail.fr>
2022-10-17 21:03:50 +0200

    Hosmer Lemeshow function

--- HosmerLemeshow.py
+++ HosmerLemeshow.py
@@ -1,40 +1,73 @@
-import pandas as pd 
+import pandas as pd
 import numpy as np
 from scipy.stats import chi2
- 
-def HosmerLemeshow(predictions,observations):
-    pihat=model.predict()
-    pihatcat=pd.cut(pihat, np.percentile(pihat,[0,25,50,75,100]),labels=False,include_lowest=True) #here we've chosen only 4 groups
 
 
-    meanprobs =[0]*4 
-    expevents =[0]*4
-    obsevents =[0]*4 
-    meanprobs2=[0]*4 
-    expevents2=[0]*4
-    obsevents2=[0]*4 
-
-    for i in range(4):
-       meanprobs[i]=np.mean(pihat[pihatcat==i])
-       expevents[i]=np.sum(pihatcat==i)*np.array(meanprobs[i])
-       obsevents[i]=np.sum(Y[pihatcat==i])
-       meanprobs2[i]=np.mean(1-pihat[pihatcat==i])
-       expevents2[i]=np.sum(pihatcat==i)*np.array(meanprobs2[i])
-       obsevents2[i]=np.sum(1-Y[pihatcat==i]) 
+def HosmerLemeshow(data, predictions, Q=10):
+    """Hosmer-Lemeshow goodness of fit test
 
 
-    data1={'meanprobs':meanprobs,'meanprobs2':meanprobs2}
-    data2={'expevents':expevents,'expevents2':expevents2}
-    data3={'obsevents':obsevents,'obsevents2':obsevents2}
-    m=pd.DataFrame(data1)
-    e=pd.DataFrame(data2)
-    o=pd.DataFrame(data3)
-    
-    # The statistic for the test, which follows, under the null hypothesis,
-    # The chi-squared distribution with degrees of freedom equal to amount of groups - 2. Thus 4 - 2 = 2
-    tt=sum(sum((np.array(o)-np.array(e))**2/np.array(e))) 
-    pvalue=1-chi2.cdf(tt,2)
+    Parameters
+    ----------
+    data : Dataframe
+        Dataframe containing :
+            - the effective of each group n
+            - for each group, the number of cases where c = 1, n1
+    predictions : Dataframe
+        Dataframe containing :
+            - for each group, the predicted proportion of c = 1 according to a logistic regression, p1
+    Q : int, optional
+        The number of groups
 
-    return pd.DataFrame([[chi2.cdf(tt,2).round(2), pvalue.round(2)]],columns = ["Chi2", "p - value"])
-    
-HosmerLemeshow(glm_full,Y)
+    Returns
+    -------
+    result : the result of the test, including Chi2-HL statistics and p-value 
+
+    """
+
+    data.loc[:, "expected_prop"] = predictions.p1
+    data = data.sort_values(by="expected_prop", ascending=False)
+
+    categories, bins = pd.cut(
+        data["expected_prop"],
+        np.percentile(data["expected_prop"], np.linspace(0, 100, Q + 1)),
+        labels=False,
+        include_lowest=True,
+        retbins=True,
+    )
+
+    meanprobs1 = np.zeros(Q)
+    expevents1 = np.zeros(Q)
+    obsevents1 = np.zeros(Q)
+    meanprobs0 = np.zeros(Q)
+    expevents0 = np.zeros(Q)
+    obsevents0 = np.zeros(Q)
+
+    y0 = data.n - data.n1
+    y1 = data.n1
+    n = data.n
+    expected_prop = predictions.p1
+    for i in range(Q):
+
+        meanprobs1[i] = np.mean(expected_prop[categories == i])
+        expevents1[i] = np.sum(n[categories == i]) * np.array(meanprobs1[i])
+        obsevents1[i] = np.sum(y1[categories == i])
+        meanprobs0[i] = np.mean(1 - expected_prop[categories == i])
+        expevents0[i] = np.sum(n[categories == i]) * np.array(meanprobs0[i])
+        obsevents0[i] = np.sum(y0[categories == i])
+
+    data1 = {"meanprobs1": meanprobs1, "meanprobs0": meanprobs0}
+    data2 = {"expevents1": expevents1, "expevents0": expevents0}
+    data3 = {"obsevents1": obsevents1, "obsevents0": obsevents0}
+    m = pd.DataFrame(data1)
+    e = pd.DataFrame(data2)
+    o = pd.DataFrame(data3)
+
+    chisq_value = sum(sum((np.array(o) - np.array(e)) ** 2 / np.array(e)))
+    pvalue = 1 - chi2.cdf(chisq_value, Q - 2)
+
+    result = pd.DataFrame(
+        [[Q - 2, chisq_value.round(2), pvalue.round(2)]],
+        columns=["df", "Chi2", "p - value"],
+    )
+    return result
