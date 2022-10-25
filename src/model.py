43d4a7f4d6195f110b677d892fb9901b3e7aa469
TristanFauvel <tristan.fauvel@hotmail.fr>
2022-10-25 22:41:30 +0200

    First submission

--- src/model.py
+++ src/model.py
@@ -10,23 +10,19 @@ try:
 except:
     raise Exception("You must provide the input path")
 
-
-
-#%%
-Warning("Remove the line below!")
-root = '/home/tristan/Desktop/Repos/DreamHF'
-#####################
-
 import numpy as np
 from preprocessing import load_data, prepare_train_test
 
 from pipeline import postprocessing, create_pipeline
-from sklearn.model_selection import cross_val_score
-# List the candidate models
+from sklearn.model_selection import cross_val_score 
 from candidate_models import candidate_models_df
+from model_evaluation import evaluate_model
 
 # Load the data
 os.environ["root_folder"] = root
+
+
+print("Processing the data...")
 pheno_df_train, pheno_df_test, readcounts_df_train, readcounts_df_test = load_data(root)
  
  
@@ -37,9 +33,8 @@ covariates = df_train.columns
 X_train, X_test, y_train, y_test,test_sample_ids = prepare_train_test(df_train, df_test, covariates)
 
 # Loop over the candidate models and optimize them individually
+print("Crossvalidation of candidate models...")
 best_score = 0
-
-#%%
 for index, row in candidate_models_df.iterrows():
     model = row["model_name"]
     monitor = row["est_monitor"]
@@ -52,18 +47,12 @@ for index, row in candidate_models_df.iterrows():
         best_model = model
         best_monitor = monitor
 
+print("Prediction with the best model...")
 best_model.fit(X_train, y_train, model__monitor=best_monitor)
 
 # Return predictions
 preds_test = best_model.predict(X_test)
 postprocessing(preds_test, test_sample_ids)
 
-# %%
-row = candidate_models_df.iloc[1,:]
-model = row["model_name"]
-monitor = row["est_monitor"]
-
-model = create_pipeline(model) 
-scores = cross_val_score(model, X_train, y_train, cv=5, error_score='raise')
-
+print("Task completed.")
 # %%
