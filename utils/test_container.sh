93f43b3d638e83e6828e15dbd22bd3e5a50605da
TristanFauvel <tristan.fauvel@hotmail.fr>
2022-10-26 19:45:20 +0200

    First submission

--- utils/test_container.sh
+++ utils/test_container.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 echo "Run container.sif, check outputs in ./singularity/test_singularity/output"
 singularity run ./singularity/test_singularity/container.sif ./singularity/test_singularity
-read -p "Proceed to the second test? " -n 1 -r
+read -p "Proceed to the second test? (y/n) " -n 1 -r
 echo    # (optional) move to a new line
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
